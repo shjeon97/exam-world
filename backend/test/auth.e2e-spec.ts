6f2ca476c35a6c92e2a4ced4132bc2eaa6be978d
shjeon97 <shjeon.dev@gmail.com>
2022-09-07 05:16:53 +0900

    20220907v2

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource, getConnection } from 'typeorm';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -10,8 +11,36 @@ describe('AuthController (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-    app.setGlobalPrefix('api');
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    // e2e 테스트가 끝나면 db를 drop해야 함
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase();
+    await connection.destroy();
+    await app.close();
+  });
+
+  describe('User 생성', () => {
+    it('User 계정이 생성되는지 확인', () => {
+      return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({
+          email: 'test@test.com',
+          name: 'test',
+          password: '12345678',
+        })
+        .expect(201);
+    });
   });
 });
