65dad550db6df7ac1c7f73553267ad174b389791
shjeon97 <shjeon.dev@gmail.com>
2022-09-09 14:25:32 +0900

    20220909v1

--- frontend/components/InputGroup.tsx
+++ frontend/components/InputGroup.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import classnames from "classnames";
 
-const InputGroup = () => {
-  return <div>inputGroup</div>;
+interface InputGroupProps {
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+const InputGroup: React.FC<InputGroupProps> = ({
+  className = "mb-2",
+  type = "text",
+  placeholder = "",
+  error,
+  value,
+  setValue,
+}) => {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        style={{ minWidth: 300 }}
+        className={classnames(
+          `w-full duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+          { "border-red-500": error }
+        )}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-500">{error}</small>
+    </div>
+  );
 };
 
 export default InputGroup;
