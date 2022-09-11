324f05c82db8a3a3af3b13f8275948302475152b
승환 전 <shjeon@acghr.co.kr>
2022-09-11 21:01:54 +0900

    20220911v2

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -26,57 +26,57 @@ export const Header: React.FC = () => {
             <a className="mr-5 hover:text-gray-900">Second Link</a>
             <a className="mr-5 hover:text-gray-900">Third Link</a>
             <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-          </nav>
-          {meData ? (
-            <>
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button>
-                    <div className="button"> {meData.name}님 </div>
-                  </Menu.Button>
-                </div>
-                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="px-1 py-1 ">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-violet-500 text-white"
-                              : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          내 정보
-                        </button>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active ? "bg-gray-100 " : "text-gray-900"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          <Image src={logout} width={20} height={20} />
-                          <div className="ml-2">로그아웃</div>
-                        </button>
-                      )}
-                    </Menu.Item>
+            {meData ? (
+              <>
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button>
+                      <div className="button"> {meData.name}님 </div>
+                    </Menu.Button>
                   </div>
-                </Menu.Items>
-              </Menu>
-            </>
-          ) : (
-            <>
-              {" "}
-              <Link href="/login">
-                <button className="inline-flex items-center bg-gray-100 border-0 py-1.5 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                  <Image src={logout} width={20} height={20} />
-                  <Button className="ml-2" name="로그인" />
-                </button>
-              </Link>
-            </>
-          )}
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            내 정보
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active ? "bg-gray-100 " : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <Image src={logout} width={20} height={20} />
+                            <div className="ml-2">로그아웃</div>
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Menu>
+              </>
+            ) : (
+              <>
+                {" "}
+                <Link href="/login">
+                  <button className="inline-flex items-center bg-gray-100 border-0 py-1.5 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                    <Image src={logout} width={20} height={20} />
+                    <Button className="ml-2" name="로그인" />
+                  </button>
+                </Link>
+              </>
+            )}
+          </nav>
         </div>
       </header>
     </>
