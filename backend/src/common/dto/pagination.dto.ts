de4aebf88bf7bcc083e7b706240150ec4075d04e
shjeon97 <shjeon.dev@gmail.com>
2022-11-07 15:03:21 +0900

    refactoring

--- backend/src/common/dto/pagination.dto.ts
+++ backend/src/common/dto/pagination.dto.ts
@@ -5,7 +5,7 @@ export class PaginationInput {
   @ApiProperty({ description: '페이지 번호' })
   page: number;
   @ApiProperty({ description: '표시할 리스트 수' })
-  pagesize: number;
+  pageSize: number;
   @ApiProperty({ description: '검색 타입', required: false })
   searchType?: string;
   @ApiProperty({ description: '검색값', required: false })
